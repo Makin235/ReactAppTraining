693f162981d2f2bc3611cf195c85eb55d6bb8bf1
Makin235 <abdelhamid.makin@gmail.com>
2024-09-05 20:25:09 +0300

    Set active selected list item.

--- ReactApp/src/components/ListGroup.tsx
+++ ReactApp/src/components/ListGroup.tsx
@@ -1,21 +1,31 @@
+import {MouseEvent, useState} from "react";
+
 function ListGroup() {
-    const items = [
+    let items = [
         'New Delhi',
         'New Yok',
         'New Jersey'
     ];
 
-    const getMsg = () => {
-        return items.length === 0 && <p>No item found</p>;
-    }
+    //Hook: data or state that changes over time.
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+
+    const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
         <>
             <h1>List</h1>
-            {getMsg()}
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                {items.map(item =>
-                    <li key={item} className="list-group-item">{item}</li>
+                {items.map((item, index) =>
+                    <li
+                        className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                        key={item}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                        }}>
+                        {item}
+                    </li>
                 )}
             </ul>
         </>
