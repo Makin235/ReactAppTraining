f0a25374879aaaa8aaa972414952002faee65d9e
Makin235 <abdelhamid.makin@gmail.com>
2024-09-06 07:43:55 +0300

    Implementing props.

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,7 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-    return <div><ListGroup/></div>
+    let items = [
+        'New Delhi',
+        'New Yok',
+        'New Jersey'
+    ];
+
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+
+    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
 }
 
 export default App;
--- ReactApp/src/components/ListGroup.tsx
+++ ReactApp/src/components/ListGroup.tsx
@@ -1,11 +1,12 @@
 import {MouseEvent, useState} from "react";
 
-function ListGroup() {
-    let items = [
-        'New Delhi',
-        'New Yok',
-        'New Jersey'
-    ];
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem}: ListGroupProps) {
 
     //Hook: data or state that changes over time.
     const [selectedIndex, setSelectedIndex] = useState(-1)
@@ -14,7 +15,7 @@ function ListGroup() {
 
     return (
         <>
-            <h1>List</h1>
+            <h1>{heading}</h1>
             {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item, index) =>
@@ -23,6 +24,7 @@ function ListGroup() {
                         key={item}
                         onClick={() => {
                             setSelectedIndex(index);
+                            onSelectItem(item);
                         }}>
                         {item}
                     </li>
