86b900a9cbbcd9ecd405138d9ab7fd6e2880c08c
Makin235 <abdelhamid.makin@gmail.com>
2024-09-09 20:20:15 +0300

    MakinTours spring project added.

---
+++ makintours/src/main/java/com/makin/makintours/MakintoursApplication.java
@@ -0,0 +1,13 @@
+package com.makin.makintours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakintoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakintoursApplication.class, args);
+	}
+
+}
---
+++ makintours/src/test/java/com/makin/makintours/MakintoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makin.makintours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakintoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
